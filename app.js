2bbe7747e3a3beb8261f3225d7d65eb3559dd94f
Mustafa Aidid <mustafa.saidid@gmail.com>
2020-12-16 04:39:13 -0500

    logic refactor

--- app.js
+++ app.js
@@ -14,6 +14,122 @@ $(document).ready(function () {
   $("#note7").val(localStorage.getItem("task7"));
   $("#note8").val(localStorage.getItem("task8"));
   $("#note9").val(localStorage.getItem("task9"));
+
+  //*******color code per time**********
+  var now = moment().hour();
+  if (now < 9) {
+    $("#note1").addClass("future");
+    $("#note2").addClass("future");
+    $("#note3").addClass("future");
+    $("#note4").addClass("future");
+    $("#note5").addClass("future");
+    $("#note6").addClass("future");
+    $("#note7").addClass("future");
+    $("#note8").addClass("future");
+    $("#note9").addClass("future");
+  } else if (now >= 18) {
+    $("#note1").addClass("past");
+    $("#note2").addClass("past");
+    $("#note3").addClass("past");
+    $("#note4").addClass("past");
+    $("#note5").addClass("past");
+    $("#note6").addClass("past");
+    $("#note7").addClass("past");
+    $("#note8").addClass("past");
+    $("#note9").addClass("past");
+  } else if (now >= 9 && now < 18) {
+    if (now === 9) {
+      $("#note1").addClass("present");
+      $("#note2").addClass("future");
+      $("#note3").addClass("future");
+      $("#note4").addClass("future");
+      $("#note5").addClass("future");
+      $("#note6").addClass("future");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 10) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("present");
+      $("#note3").addClass("future");
+      $("#note4").addClass("future");
+      $("#note5").addClass("future");
+      $("#note6").addClass("future");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 11) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("present");
+      $("#note4").addClass("future");
+      $("#note5").addClass("future");
+      $("#note6").addClass("future");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 12) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("present");
+      $("#note5").addClass("future");
+      $("#note6").addClass("future");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 13) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("past");
+      $("#note5").addClass("present");
+      $("#note6").addClass("future");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 14) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("past");
+      $("#note5").addClass("past");
+      $("#note6").addClass("present");
+      $("#note7").addClass("future");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 15) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("past");
+      $("#note5").addClass("past");
+      $("#note6").addClass("past");
+      $("#note7").addClass("present");
+      $("#note8").addClass("future");
+      $("#note9").addClass("future");
+    } else if (now === 16) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("past");
+      $("#note5").addClass("past");
+      $("#note6").addClass("past");
+      $("#note7").addClass("past");
+      $("#note8").addClass("present");
+      $("#note9").addClass("future");
+    } else if (now === 17) {
+      $("#note1").addClass("past");
+      $("#note2").addClass("past");
+      $("#note3").addClass("past");
+      $("#note4").addClass("past");
+      $("#note5").addClass("past");
+      $("#note6").addClass("past");
+      $("#note7").addClass("past");
+      $("#note8").addClass("past");
+      $("#note9").addClass("present");
+    }
+  }
 });
 
 //********clear local storage and reload*********
@@ -67,119 +183,3 @@ $("#saveNote9").click(function () {
   let task = $("#note9").val();
   localStorage.setItem("task9", task);
 });
-
-//*******color code per time**********
-var now = moment().hour();
-if (now < 9) {
-  $("#note1").addClass("future");
-  $("#note2").addClass("future");
-  $("#note3").addClass("future");
-  $("#note4").addClass("future");
-  $("#note5").addClass("future");
-  $("#note6").addClass("future");
-  $("#note7").addClass("future");
-  $("#note8").addClass("future");
-  $("#note9").addClass("future");
-} else if (now >= 18) {
-  $("#note1").addClass("past");
-  $("#note2").addClass("past");
-  $("#note3").addClass("past");
-  $("#note4").addClass("past");
-  $("#note5").addClass("past");
-  $("#note6").addClass("past");
-  $("#note7").addClass("past");
-  $("#note8").addClass("past");
-  $("#note9").addClass("past");
-} else if (now >= 9 && now < 18) {
-  if (now === 9) {
-    $("#note1").addClass("present");
-    $("#note2").addClass("future");
-    $("#note3").addClass("future");
-    $("#note4").addClass("future");
-    $("#note5").addClass("future");
-    $("#note6").addClass("future");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 10) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("present");
-    $("#note3").addClass("future");
-    $("#note4").addClass("future");
-    $("#note5").addClass("future");
-    $("#note6").addClass("future");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 11) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("present");
-    $("#note4").addClass("future");
-    $("#note5").addClass("future");
-    $("#note6").addClass("future");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 12) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("present");
-    $("#note5").addClass("future");
-    $("#note6").addClass("future");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 13) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("past");
-    $("#note5").addClass("present");
-    $("#note6").addClass("future");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 14) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("past");
-    $("#note5").addClass("past");
-    $("#note6").addClass("present");
-    $("#note7").addClass("future");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 15) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("past");
-    $("#note5").addClass("past");
-    $("#note6").addClass("past");
-    $("#note7").addClass("present");
-    $("#note8").addClass("future");
-    $("#note9").addClass("future");
-  } else if (now === 16) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("past");
-    $("#note5").addClass("past");
-    $("#note6").addClass("past");
-    $("#note7").addClass("past");
-    $("#note8").addClass("present");
-    $("#note9").addClass("future");
-  } else if (now === 17) {
-    $("#note1").addClass("past");
-    $("#note2").addClass("past");
-    $("#note3").addClass("past");
-    $("#note4").addClass("past");
-    $("#note5").addClass("past");
-    $("#note6").addClass("past");
-    $("#note7").addClass("past");
-    $("#note8").addClass("past");
-    $("#note9").addClass("present");
-  }
-}
